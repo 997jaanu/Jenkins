ba0bdb9d7572395edc93e27aa05b4e00e7e31bfd
997jaanu <997jaanu@gmail.com>
2024-12-13 17:10:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "google_storage_bucket" "storage-bucket" {
   public_access_prevention = "enforced"
 }
 #
+resource "google_storage_bucket" "storage-bucket" {
+  name                     = "s-bucket-002"
+  project                  = "windy-album-444116-g9"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+#
